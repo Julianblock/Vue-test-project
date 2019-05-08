02279c3f5018804d0dc381219d6666cd2b9d858e
Julianblock <Julian@zennify.com>
2019-05-08 15:33:34 -0700

    added new functionality

--- my-app/src/components/Samples.vue
+++ my-app/src/components/Samples.vue
@@ -11,7 +11,7 @@
 	</div>
 	
 	<ul class="people-list">
-		<li  v-for="person in filteredPeople" v-bind:key="person" >{{ person.name }}</li>
+		<li  v-for="person in filteredPeople" v-bind:key='person' >{{ person.name }}</li>
 	</ul>
   <div class='ehealth-row'>
     <div class="ehealth-row-tile">
@@ -52,8 +52,53 @@
       </div>
     </div>
   </div>
+    <Search />
 </div>
+
+
+
+
 </div>
 </template>
 
 
+<script>
+import Search from './search.vue';
+
+export default {
+  components: {
+    Search
+  },
+  el:  "people",
+    data () {
+      return {
+      people: [
+        { name: "Bill Gates", category: "Tech" },
+        { name: "Steve Jobs", category: "Tech" },
+        { name: "Jeff Bezos", category: "Tech" },
+        { name: "George Clooney", category: "Entertainment" },
+        { name: "Meryl Streep", category: "Entertainment" },
+        { name: "Amy Poehler", category: "Entertainment" },
+        { name: "Lady of Lórien", category: "Fictional" },
+        { name: "BB8", category: "Fictional" },
+        { name: "Michael Scott", category: "Fictional" }
+      ],
+      selectedCategory: "All"
+      }
+    },
+    computed: {
+      filteredPeople: function() {
+        var vm = this;
+        var category = vm.selectedCategory;
+        
+        if(category === "All") {
+          return vm.people;
+        } else {
+          return vm.people.filter(function(person) {
+            return person.category === category;
+          });
+        }
+      }
+    }
+  }
+</script>
